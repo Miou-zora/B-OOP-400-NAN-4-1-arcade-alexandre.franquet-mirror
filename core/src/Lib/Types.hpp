9a5d60d093f3f58992fde8d3e0f98e3ff98f6be5
quentin <Quentin.brejoin@gmail.com>
2023-04-05 12:48:52 +0200

    refacto(core): refacto inputs (#38)
    * refacto(core): refacto inputs
    
    * refacto(core): inouts
    
    * refacto(core): inouts
    
    * update inputs
    
    ---------
    
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>

--- core/src/Lib/Types.hpp
+++ core/src/Lib/Types.hpp
@@ -17,19 +17,49 @@ namespace Arcade {
     };
 
     enum Inputs {
-        /*Those are for in game inputs for player movement */
         IKEY_UP,
         IKEY_DOWN,
         IKEY_LEFT,
         IKEY_RIGHT,
         IKEY_SPACE,
+        IKEY_ENTER,
+        IKEY_BACKSPACE,
+        IKEY_TAB,
+        IKEY_SHIFT,
+        IKEY_CTRL,
+        IKEY_ALT,
+
+        /* Globals inputs */
+        IKEY_B, //Graphical library
+        IKEY_D, //Game library
+        IKEY_S, //QUIT
+        IKEY_M, //MENU
 
         /*Those key are for arcade control Exit/Menu/... */
         IKEY_Q,
         IKEY_ESC,
+        IKEY_A,
+        IKEY_C,
+        IKEY_E,
+        IKEY_F,
         IKEY_G,
         IKEY_H,
 
+        IKEY_I,
+        IKEY_J,
+        IKEY_K,
+        IKEY_L,
+        IKEY_N,
+        IKEY_O,
+        IKEY_P,
+        IKEY_R,
+        IKEY_T,
+        IKEY_U,
+        IKEY_V,
+        IKEY_W,
+        IKEY_X,
+        IKEY_Y,
+        IKEY_Z,
         NO_KEY
     };
 
