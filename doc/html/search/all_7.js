704845774c16870e1a2391bb32520ab23aec2f4a
Miouzora <91665686+Miou-zora@users.noreply.github.com>
2023-04-07 19:14:51 +0200

    doc(README.md): add contributors with main characters (#67)
    * doc(README.md): add contributors with main characters
    
    * doc(Pdf): add pdf with site

---
+++ doc/html/search/all_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['load_0',['load',['../classArcade_1_1IGame.html#a16b7e26d7787105e9f87f377df9b4c6e',1,'Arcade::IGame']]]
+];
---
+++ doc/html/search/all_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['render_0',['render',['../classArcade_1_1IGame.html#a68c2fc4d1287815b0b3d74259f99bb1e',1,'Arcade::IGame']]],
+  ['renderwindow_1',['renderWindow',['../classArcade_1_1ILib.html#acf91b8e1c3838255664aca2d81adaaeb',1,'Arcade::ILib']]],
+  ['reset_2',['reset',['../classArcade_1_1IGame.html#a1ec25d36eab342e71e5ed56555b6a618',1,'Arcade::IGame']]]
+];
