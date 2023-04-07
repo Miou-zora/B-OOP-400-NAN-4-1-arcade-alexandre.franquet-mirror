04dc60d4aa3e7780cb4a8125ddd240864a5387bc
miou-zora <alexandre.franquet@epitech.eu>
2023-04-07 19:12:27 +0200

    doc(Pdf): add pdf with site

---
+++ doc/html/search/all_6.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['igame_0',['IGame',['../classArcade_1_1IGame.html',1,'Arcade::IGame'],['../classArcade_1_1IGame.html#a9d430c870e35c4260a2a42be7e7744b8',1,'Arcade::IGame::IGame()']]],
+  ['ilib_1',['ILib',['../classArcade_1_1ILib.html',1,'Arcade::ILib'],['../classArcade_1_1ILib.html#a7191d06c1da2adc8f4e7056ae6a947c2',1,'Arcade::ILib::ILib()']]],
+  ['iobject_2',['IObject',['../classArcade_1_1IObject.html',1,'Arcade::IObject'],['../classArcade_1_1IObject.html#a94f8eace1d8320b7a35918eae542f1f0',1,'Arcade::IObject::IObject()']]],
+  ['iskeypressed_3',['isKeyPressed',['../classArcade_1_1ALib.html#a535800c22074d6316c2f8cba0d273b18',1,'Arcade::ALib::isKeyPressed()'],['../classArcade_1_1ILib.html#a39b4e7757688e1c5eebef2b69fb5e96b',1,'Arcade::ILib::isKeyPressed()']]],
+  ['iskeyreleased_4',['isKeyReleased',['../classArcade_1_1ALib.html#a6e4b95e406584ef026a49d11b325a62e',1,'Arcade::ALib::isKeyReleased()'],['../classArcade_1_1ILib.html#ab6a4e6bd47ee2f71722baf36c85a8b36',1,'Arcade::ILib::isKeyReleased(Arcade::Inputs input)=0']]],
+  ['iswindowclosed_5',['isWindowClosed',['../classArcade_1_1ILib.html#ae96901819ce79b55e6d5ecc7eebba33f',1,'Arcade::ILib']]]
+];
---
+++ doc/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['base_20architecture_20for_20the_20arcade_20project_0',['Base Architecture for the Arcade Project',['../index.html',1,'']]]
+];
