6d52d3d8f52c8ac8fed110781b981be1fa9eddf6
miou-zora <alexandre.franquet@epitech.eu>
2023-04-07 19:15:27 +0200

    doc(Doxygen): add all class in doc

--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['text_0',['Text',['../classArcade_1_1Text.html',1,'Arcade']]]
+  ['igame_0',['IGame',['../classArcade_1_1IGame.html',1,'Arcade']]],
+  ['ilib_1',['ILib',['../classArcade_1_1ILib.html',1,'Arcade']]],
+  ['iobject_2',['IObject',['../classArcade_1_1IObject.html',1,'Arcade']]]
 ];
--- doc/html/search/functions_4.js
+++ doc/html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['load_0',['load',['../classArcade_1_1IGame.html#a16b7e26d7787105e9f87f377df9b4c6e',1,'Arcade::IGame']]]
+  ['load_0',['load',['../classArcade_1_1IGame.html#a16b7e26d7787105e9f87f377df9b4c6e',1,'Arcade::IGame']]],
+  ['loadgraphicallib_1',['loadGraphicalLib',['../classArcade_1_1LibLoader.html#aea0cac002cc9afad1c073b05a1eab435',1,'Arcade::LibLoader']]]
 ];
