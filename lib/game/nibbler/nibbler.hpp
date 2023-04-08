c99937d9bfefee4c353188fb771ff9679bbbc109
guylain.thomas <guylain.thomas@epitech.eu>
2023-04-08 22:22:56 +0200

    feat(nibbler) : movements if collisions with wall

--- lib/game/nibbler/nibbler.hpp
+++ lib/game/nibbler/nibbler.hpp
@@ -63,7 +63,10 @@ namespace Arcade {
 
             void loadHighScore();
             int checkWinGame();
-
+            std::pair<int, int> updateHeadPos();
+            bool checkIfAutomaticMovePossible(int x, int y);
+            bool moveFreeDir(int x, int y);
+            bool checkOldDirIsPossible();
 
         private:
             std::vector<std::shared_ptr<Arcade::AObject>> _mapObjects;
@@ -73,6 +76,7 @@ namespace Arcade {
             std::vector<std::string> _snake;
             std::vector<std::shared_ptr<Arcade::AObject>> _foodObjects;
             std::shared_ptr<Arcade::Text> _scoreText;
+            std::pair<int, int> _headPos;
             char _food;
             bool _isAlive;
             int _score;
